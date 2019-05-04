f918d976df3c65eb253baf45204f1c5e8a345089
Duc Pham <ducpham1098@gmail.com>
2019-05-05 00:04:06 +0700

    Updates

---
+++ precache-manifest.8fbd412469b0b55895aaf44a00c7b084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe1c4fdbaf9b0e2ece4f24e5236403f",
+    "url": "/To-do-List-with-React-Firebase/index.html"
+  },
+  {
+    "revision": "5a93948b74186e548aa5",
+    "url": "/To-do-List-with-React-Firebase/static/css/main.c5fcc091.chunk.css"
+  },
+  {
+    "revision": "ba2bb56c366a7fbd92e1",
+    "url": "/To-do-List-with-React-Firebase/static/js/2.0d162cbb.chunk.js"
+  },
+  {
+    "revision": "5a93948b74186e548aa5",
+    "url": "/To-do-List-with-React-Firebase/static/js/main.1083a6d2.chunk.js"
+  },
+  {
+    "revision": "55b0d951175ec81e084c",
+    "url": "/To-do-List-with-React-Firebase/static/js/runtime~main.a7e976d9.js"
+  }
+]);
